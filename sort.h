49813cb3eec79f20ac9c0243ef68ea3f531bac5e
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-19 01:37:26 -0700

    prototype update for 3. Quick sort task

--- sort.h
+++ sort.h
@@ -26,5 +26,8 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_rec(int *array, int lower, int higher, size_t size);
+int lomuto_partition(int *array, int lower, int higher, size_t size);
 
 #endif
