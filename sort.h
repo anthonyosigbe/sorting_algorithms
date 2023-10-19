1f3e3198cceecbb0158c7b5631e1841e32122ac0
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-19 02:11:26 -0700

    5. Cocktail shaker sort tasks completed

--- sort.h
+++ sort.h
@@ -31,5 +31,8 @@ void quick_sort_rec(int *array, int lower, int higher, size_t size);
 int lomuto_partition(int *array, int lower, int higher, size_t size);
 void counting_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
 
 #endif
