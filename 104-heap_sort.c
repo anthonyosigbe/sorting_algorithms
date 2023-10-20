f6a25b2108a9448160729bb06ee31ad246e321f0
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-20 03:46:39 -0700

    bug and betty styling fix

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,13 +1,8 @@
 #include "sort.h"
 
-void swap_ints(int *a, int *b);
-void max_heapify(int *array, size_t size, size_t base, size_t root);
-void heap_sort(int *array, size_t size);
-
 /**
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
- * 
  * @b: The second integer to swap.
  */
 void swap_ints(int *a, int *b)
